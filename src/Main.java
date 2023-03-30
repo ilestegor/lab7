4208587a82834b958f17f1ec2d882e9d9e4c8b5f
ilestegor <99200163+ilestegor@users.noreply.github.com>
2023-03-30 19:47:33 +0300

    Completed all edits

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,13 @@ import manager.UserManager;
 import parse.YamlReader;
 import utility.Printer;
 
+import javax.xml.stream.XMLStreamWriter;
 import java.time.LocalDateTime;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("---------- " + LocalDateTime.now().toString().substring(0, 10) + " ---------");
+        Printer printer = new Printer();
+        printer.printNextLine("---------- " + LocalDateTime.now().toString().substring(0, 10) + " ---------");
         CollectionManager collectionManager = new CollectionManager();
         YamlReader yamlReader = new YamlReader(new Printer());
         collectionManager.readToCollection(yamlReader);
